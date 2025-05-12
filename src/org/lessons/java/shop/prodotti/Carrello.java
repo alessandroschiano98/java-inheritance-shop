5025bf8141aa72e7626e007a4fc4f80653c6e318
alessandroschiano98 <schianoski98@gmail.com>
2025-05-12 11:51:18 +0200

    inizializziamo la classe "Carrello"

---
+++ src/org/lessons/java/shop/prodotti/Carrello.java
@@ -0,0 +1,17 @@
+package org.lessons.java.shop.prodotti;
+
+import java.util.Scanner;
+
+public class Carrello {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci uno dei seguenti prodotti: CUFFIE - SMARTPHONE - TELEVISORI");
+
+
+
+
+        
+        input.close();
+
+    }
+}
